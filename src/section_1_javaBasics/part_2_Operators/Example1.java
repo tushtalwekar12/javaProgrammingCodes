b67b841e302387aead8679e23f48c73f8675aeb2
Tushar Talwekar <tushartalwekar1234@gmail.com>
2025-06-03 21:22:16 +0530

    adding files

---
+++ src/section_1_javaBasics/part_2_Operators/Example1.java
@@ -0,0 +1,9 @@
+package section_1_javaBasics.part_2_Operators;
+// Airthmetic Operator
+public class Example1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        System.out.println(a+b);
+    }
+}
