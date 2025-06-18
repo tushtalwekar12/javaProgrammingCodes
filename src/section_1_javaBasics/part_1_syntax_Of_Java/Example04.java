8452bf9c6e5e1ad8e7de3499c1803482ac4c0023
Tushar Talwekar <tushartalwekar1234@gmail.com>
2025-06-18 16:33:08 +0530

    adding files

--- src/section_1_javaBasics/part_1_syntax_Of_Java/Example04.java
+++ src/section_1_javaBasics/part_1_syntax_Of_Java/Example04.java
@@ -1,4 +1,18 @@
 package section_1_javaBasics.part_1_syntax_Of_Java;
+// Input form the users
+import java.util.Scanner;
 
 public class Example04 {
+    public static void main(String[] args) {
+        // Add two number and print Sum
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first number : ");
+        int num1 =input.nextInt();
+        System.out.println("Enter the second number : ");
+        int num2 = input.nextInt();
+
+        int sum =num1 + num2;
+        System.out.println("The Sum is : "+ sum);
+    }
 }
--- src/section_1_javaBasics/part_2_Operators/Example1.java
+++ src/section_1_javaBasics/part_2_Operators/Example1.java
@@ -1,9 +1,23 @@
 package section_1_javaBasics.part_2_Operators;
-// Airthmetic Operator
+// Operator
+import java.util.Scanner;
 public class Example1 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 12;
-        System.out.println(a+b);
+        //1. Airthmetic Operator - Used for basic mathematical operations. (+,-,*,/,%)
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        System.out.println("Sum of a + b : "+ (a+b));
+        System.out.println("Sub of a - b : "+ (a-b));
+        System.out.println("Mul of a * b : "+ (a*b));
+        System.out.println("Div of a / b : "+ (a/b));
+        System.out.println("Mod of a % b : "+ (a % b));
+
+
+        //2. Assignment Operator -Used to assign values to variable. (=,+=,-=,*=,/=)\
+        int c = 10;
+         c += 2;
+        System.out.println(c);
+
     }
 }
