df1741183a810ac2629d6ea1e49432ce75b8e87a
Tushar Talwekar <tushartalwekar1234@gmail.com>
2025-08-01 11:18:36 +0530

    updated code

---
+++ src/section_1_javaBasics/part_1_syntax_Of_Java/Example00_HelloWorld.java
@@ -0,0 +1,7 @@
+package section_1_javaBasics.part_1_syntax_Of_Java;
+
+public class Example00_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
